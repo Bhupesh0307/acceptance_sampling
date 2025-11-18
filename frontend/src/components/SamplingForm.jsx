7de1d5d5f8053e3d168de0cfe15047abe9d066c9
Bhupesh Jha <shrutijha0803@gmail.com>
2025-11-19 02:39:47 +0530

    Delete frontend directory

--- frontend/src/components/SamplingForm.jsx
+++
@@ -1,7 +0,0 @@
-// For single sampling
-import SingleSamplingForm from './SingleSamplingForm';
-
-// For double sampling  
-import DoubleSamplingForm from './DoubleSamplingForm';
-
-{planType === "Single" ? <SingleSamplingForm /> : <DoubleSamplingForm />}
